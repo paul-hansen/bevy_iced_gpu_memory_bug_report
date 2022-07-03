3c7516cf8733e5775464b61e90bb1221e20c673a
Paul <mail@paul.rs>
2022-07-03 07:33:39 -0500

    Show the problem just using a button without the color picker

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This repo replicates a bug when using [bevy_iced](https://crates.io/crates/bevy_iced) with the color picker from [iced_aw](https://crates.io/crates/iced_aw) where the gpu shared memory maxes out and crashes.
+This repo replicates a bug when using [bevy_iced](https://crates.io/crates/bevy_iced) where the gpu shared memory slowly increases over time even when showing just a simple button and eventually runs out of memory and crashes. It may take a long time to crash on GPUs with a lot of ram. I saw my memory go from 0.1 GB to 0.5 GB in about 5 minutes. It would probably take 3 ish hours to crash at that rate.
 
 The bug is reported here: https://github.com/tasgon/bevy_iced/issues/1
 
--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,15 @@ use bevy_iced::iced::{
     Element, Program,
 };
 use bevy_iced::{IcedAppExtensions, IcedPlugin};
-use iced_aw::{color_picker, ColorPicker};
 
 #[derive(Debug, Clone)]
 pub enum UiMessage {
-    OpenColorPicker,
-    CloseColorPicker,
-    ColorPicked(iced_native::Color),
+    ButtonPressed,
 }
 
 #[derive(Default)]
 pub struct MainUi {
-    color_picker_state: color_picker::State,
-    color_picker_btn_state: button::State,
+    button_state: button::State,
 }
 
 impl Program for MainUi {
@@ -25,22 +21,15 @@ impl Program for MainUi {
 
     fn update(&mut self, msg: UiMessage) -> iced_native::Command<UiMessage> {
         match msg {
-            UiMessage::OpenColorPicker => self.color_picker_state.show(true),
-            UiMessage::CloseColorPicker => self.color_picker_state.show(false),
-            UiMessage::ColorPicked(_) => self.color_picker_state.show(false),
+            UiMessage::ButtonPressed => println!("test"),
         }
         iced_native::Command::none()
     }
 
     fn view(&mut self) -> Element<'_, Self::Message, Self::Renderer> {
-        ColorPicker::new(
-            &mut self.color_picker_state,
-            Button::new(&mut self.color_picker_btn_state, Text::new("Pick color"))
-                .on_press(UiMessage::OpenColorPicker),
-            UiMessage::CloseColorPicker,
-            UiMessage::ColorPicked,
-        )
-        .into()
+        Button::new(&mut self.button_state, Text::new("Test Button"))
+            .on_press(UiMessage::ButtonPressed)
+            .into()
     }
 }
 
